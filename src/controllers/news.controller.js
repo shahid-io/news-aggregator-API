28b9e4e76de978d8bcb08b1132f156d221224122
Shahid Raza <50213579+shahidGithub532@users.noreply.github.com>
2023-11-30 22:44:59 +0530

    feat: News API

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -9,25 +9,29 @@ const { NewsAPIConfig } = require('../config');
 const { default: axios } = require('axios');
 const userService = new UserService();
 
+// const params = {
+        //     apiKey: NewsAPIConfig.NEWS_API_KEY,
+        //     category: preferences.join(','),
+        // };
+
 /** Get news based on user preferences */
 const getNews = async (req, res) => {
     try {
         const loggedInUser = await userService.isAuthenticated(req.headers['x-access-token']);
         const preferences = await userService.getUserNewsPreferences(loggedInUser);
-        const params = {
+        const randomPreference = preferences[Math.floor(Math.random() * preferences.length)];
+        const params= {
             apiKey: NewsAPIConfig.NEWS_API_KEY,
-            category: preferences.join(','),
+            q: randomPreference, 
         };
+        console.log(params)
         const response = await axios.get(NewsAPIConfig.NEWS_API_URL, { params });
-        console.log('response', response)
-        console.log('response', response.data.articles)
         SuccessResponse.data = response.data.articles;
         SuccessResponse.message = "News Fetched Successfully";
         return res.status(StatusCodes.OK).json(SuccessResponse);
     } catch (error) {
         ErrorResponse.error = error;
         ErrorResponse.message = error.message;
-        // return res.status(error.statusCode).json(ErrorResponse);
         return res.status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
